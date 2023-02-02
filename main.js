5c190c16475f65b581543370e22f98ffd78d532e
Satyam0401 <dsingh96340@gmail.com>
2023-02-02 18:53:55 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const form = document.querySelector("#book-form")
+
+/*function sum(a,b)
+return a+b
+
+const sum = (a,b)=>{//without braces if in one line
+    return a+b
+}*/
+
+form.addEventListener("submit",(e)=>{
+    e.preventDefault()
+    console.log("hii")
+})
